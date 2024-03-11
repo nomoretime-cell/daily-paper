cc801989a45f66caf1e6ba633f51fef4f1637daa
遥奕 <jibing.yjb@alibaba-inc.com>
2024-03-12 00:59:58 +0800

    [feature] [taskId]: [taskUrl]: [describe]: support download pdf from arxive

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
 import feedparser
-from datetime import datetime, timedelta
+import requests
 
-# 构造arXiv的RSS订阅源URL，这里以计算机科学为例
-category = "cs"
+# cs, physics, math, econ
+category = "cs.AI"
+# feed_url = f"http://rss.arxiv.org/rss/{category}"
 feed_url = f"http://export.arxiv.org/rss/{category}"
 
-# 解析订阅源
-feed = feedparser.parse(feed_url)
 
-# 获取一周前的日期
-one_week_ago = datetime.now() - timedelta(days=7)
+class arXiv:
+    def __init__(self, feed_url):
+        self.feed = feedparser.parse(feed_url)
 
-print(f"arXiv论文 - {feed.feed.title}\n")
+    def get_pdf_link(self, link):
+        return link.replace("abs", "pdf")
 
-# 遍历并打印最近一周的条目
-for entry in feed.entries:
-    print(f"标题: {entry.title}")
-    print(f"链接: {entry.link}")
-    print()
+    def download_pdf(self, url, filename):
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(filename, "wb") as file:
+                file.write(response.content)
+            print(f"Downloaded '{filename}'.")
+        else:
+            print(f"Failed to download file from {url}.")
+
+    def process(self):
+        print(f"arXiv feed title - {self.feed.feed.title}\n")
+        print(f"arXiv feed published time - {self.feed.feed.published}\n")
+        for entry in self.feed.entries:
+            print(f"title: {entry.title}")
+            print(f"author: {entry.author}")
+            print(f"summary: {entry.summary}")
+            print(f"link: {entry.link}")
+            print("-" * 100)
+            print()
+            pdf_url = self.get_pdf_link(entry.link)
+            if pdf_url:
+                arxiv_id = entry.id.split("/")[-1]
+                filename = f"{arxiv_id}.pdf"
+                self.download_pdf(pdf_url, filename)
+
+
+if __name__ == "__main__":
+    arxiv = arXiv(feed_url)
+    arxiv.process()
